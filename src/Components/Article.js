0d5431d306eadcc19151de8c0cc3a741a0b43b4a
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-05-15 13:22:27 +0200

    snapshot article done

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -2,7 +2,9 @@
 
 import React from 'react'
 
-const Article = () => <div>Article</div>
+const Article = () => <div>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Zinedine_Zidane_by_Tasnim_03.jpg/280px-Zinedine_Zidane_by_Tasnim_03.jpg" alt="zidane"/>
+</div>
 
 
 export default Article
